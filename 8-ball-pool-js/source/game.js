d6c41a957235bececa4f8895b85a94e6600cd8b7
JonathanGoncalvesDeOliveira <jonathan.g.oliveira13@gmail.com>
2019-12-23 18:35:47 -0300

    começando a fazer um jogo de bilhar

---
+++ 8-ball-pool-js/source/game.js
@@ -0,0 +1,23 @@
+function Game(){
+
+}
+
+Game.prototype.init = function(){
+    this.gameWorld = new GameWorld();
+}
+
+Game.prototype.start = function(){
+    poolGame.init();
+    poolGame.mainLoop();
+}
+
+Game.prototype.mainLoop = function(){
+    canvas.clear();
+    poolGame.gameWorld.update();
+    poolGame.gameWorld.draw();
+    
+    requestAnimationFrame(poolGame.mainLoop);
+
+}
+
+let poolGame = new Game();
--- README.md
+++ README.md
@@ -8,3 +8,5 @@ https://aprendizzz.github.io/games-testes-js/system-particle-test/
 https://aprendizzz.github.io/games-testes-js/physics-test/
 
 https://aprendizzz.github.io/games-testes-js/Normalizacao/
+
+https://aprendizzz.github.io/games-testes-js/8-ball-pool-js/
